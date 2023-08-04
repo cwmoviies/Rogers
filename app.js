5f9227445914c76bc2fbba01fcf971269385453f
cwmoviies <141397861+cwmoviies@users.noreply.github.com>
2023-08-04 02:20:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_gq7an17", "template_xe7frwd", formData)
+    emailjs.send("service_jc27f8i", "template_mfnpra6", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
